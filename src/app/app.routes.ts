abd9ce3703dd9e7820a67e8e923179974ec5a0d6
Andres Alba <bryanalba92@gmail.com>
2019-08-21 13:08:53 -0500

    Creación del componente register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
 
 
 
@@ -24,7 +25,7 @@ const appRoutes: Routes = [
       ]
     },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: DashboardComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent }
 ];
 
