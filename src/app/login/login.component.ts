358475e8cc87be935b0b348d85fcddaa4e742b73
Andres Alba <bryanalba92@gmail.com>
2019-08-25 19:10:58 -0500

    Uso de Scripts de archivos importados en el index.html en TypeScript

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare function init_plugins(); // de esta manera se puede llamar cualquier script que se encuentre fuera de
+// angular en un archivo de javascript, esto funciona con plugins, carruseles,  tooltips
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( public router: Router) { }
 
   ngOnInit() {
+    init_plugins();
+  }
+
+  ingresar() {
+    this.router.navigate(['/dashboard']);
   }
 
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare function init_plugins(); // de esta manera se puede llamar cualquier script que se encuentre fuera de
+// angular en un archivo de javascript, esto funciona con plugins, carruseles,  tooltips
 
 @Component({
   selector: 'app-pages',
@@ -10,6 +12,7 @@ export class PagesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    init_plugins();
   }
 
 }
