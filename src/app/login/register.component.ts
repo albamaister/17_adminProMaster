14142dfaf87830181ccec4edde40c2486c4974b0
Andres Alba <bryanalba92@gmail.com>
2019-08-29 22:38:45 -0500

    Creación de modelo de usuario-crear usuarios desde frontend y validando que las contraseñas sean iguales

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+declare function init_plugins(); // de esta manera se puede llamar cualquier script que se encuentre fuera de
+// angular en un archivo de javascript, esto funciona con plugins, carruseles,  tooltips
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  forma: FormGroup;
+
   constructor() { }
 
+  sonIguales( group: FormGroup ) {
+
+    return group.get('password').value === group.get('password2').value
+    ? null : {'sonIguales': true};
+
+  }
+
   ngOnInit() {
+    init_plugins();
+    this.forma = new FormGroup({
+      nombre: new FormControl(null, Validators.required),
+      correo: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required),
+      password2: new FormControl(null, Validators.required),
+      condiciones: new FormControl( false )
+    }, { validators: this.sonIguales});
+
+    this.forma.setValue({
+      nombre: 'Test ',
+      correo: 'test@test.com',
+      password: '123456',
+      password2: '123456',
+      condiciones: true
+    });
+  }
+
+  registrarUsuario() {
+
+    if ( this.forma.invalid ) {
+      return;
+    }
+
+    if ( !this.forma.value.condiciones ) {
+
+      console.log('Debe de aceptar las condiciones');
+      return;
+
+    }
+
+    console.log(this.forma.value);
   }
 
 }
