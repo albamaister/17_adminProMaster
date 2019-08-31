a8e296e2e2dcbfb2d4a7fc29b925a73d5db3668a
Andres Alba <bryanalba92@gmail.com>
2019-08-31 00:00:09 -0500

    Obtener el Token de una autenticación con el Google SignIn

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,8 @@ import { Usuario } from '../models/usuario.model';
 declare function init_plugins(); // de esta manera se puede llamar cualquier script que se encuentre fuera de
 // angular en un archivo de javascript, esto funciona con plugins, carruseles,  tooltips
 
+declare const gapi: any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -15,11 +17,41 @@ declare function init_plugins(); // de esta manera se puede llamar cualquier scr
 export class LoginComponent implements OnInit {
 
   recuerdame = false;
+  email: string;
+  auth2: any;
 
   constructor( public router: Router, public _usuarioService: UsuarioService ) { }
 
   ngOnInit() {
     init_plugins();
+    this.googleInit();
+    this.email = localStorage.getItem('email') || '';
+
+    if ( this.email.length > 1 ) {
+      this.recuerdame = true;
+    }
+  }
+
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '880925857002-298q36627seve6u0t9v7691j00o88ect.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+
+      this.attachSignin( document.getElementById('btnGoogle') );
+
+
+    });
+  }
+
+  attachSignin( element ) {
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+      // let profile = googleUser.getBasicProfile();
+      let token = googleUser.getAuthResponse().id_token;
+      console.log( token );
+    });
   }
 
   ingresar( forma: NgForm ) {
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -19,6 +19,14 @@ export class UsuarioService {
 
    login( usuario: Usuario, recordar: boolean = false) {
 
+    if ( recordar ) {
+
+      localStorage.setItem('email', usuario.email);
+
+    } else {
+      localStorage.removeItem('email');
+    }
+
     const url = URL_SERVICIOS + '/login';
 
     return this.http.post( url, usuario ).pipe(
