b2a36a3b8e0f43acdabc1f0d4413e662ddafa8a5
Andres Alba <bryanalba92@gmail.com>
2019-09-05 00:14:55 -0500

    Actualizar la fotografia y el registro de un médico

--- src/app/pages/medicos/medico.component.ts
+++ src/app/pages/medicos/medico.component.ts
@@ -5,6 +5,7 @@ import { MedicoService } from '../../services/service.index';
 import { HospitalService } from 'src/app/services/service.index';
 import { Medico } from '../../models/medico.model';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ModalUploadService } from '../../components/modal-upload/modal-upload.service';
 
 @Component({
   selector: 'app-medico',
@@ -21,7 +22,8 @@ export class MedicoComponent implements OnInit {
   constructor(public _medicoService: MedicoService,
               public _hospitalService: HospitalService,
               public router: Router,
-              public activatedRoute: ActivatedRoute) {
+              public activatedRoute: ActivatedRoute,
+              public _modalUploadService: ModalUploadService) {
 
                 activatedRoute.params.subscribe(params => {
                   let id = params['id'];
@@ -35,6 +37,11 @@ export class MedicoComponent implements OnInit {
 
     this._hospitalService.cargarHospitales()
         .subscribe( hospitales => this.hospitales = hospitales );
+
+    this._modalUploadService.notificacion
+        .subscribe( resp => {
+            this.medico.img = resp.medico.img;
+          });
 
   }
 
@@ -66,6 +73,12 @@ export class MedicoComponent implements OnInit {
             this.medico.hospital = medico.hospital._id;
             this.cambioHospital(this.medico.hospital);
           });
+  }
+
+  cambiarFoto() {
+
+    this._modalUploadService.mostrarModal('medicos', this.medico._id);
+
   }
 
 }
--- src/app/services/medico/medico.service.ts
+++ src/app/services/medico/medico.service.ts
@@ -52,15 +52,31 @@ export class MedicoService {
   guardarMedico( medico: Medico ) {
 
     let url = URL_SERVICIOS + '/medico';
-    url += '?token=' + this._usuarioService.token;
-    return this.http.post(url, medico)
-        .pipe(
-          map( (resp: any) => {
 
-            swal('Created Doctor', medico.nombre, 'success');
-            return resp.medico;
-          })
-        );
+    if ( medico._id ) {
+      // actualizando
+      url += '/' + medico._id;
+      url += '?token=' + this._usuarioService.token;
+      return this.http.put(url, medico)
+            .pipe(
+              map( (resp: any) => {
+                swal('Updated Doctor', medico.nombre, 'success');
+                return resp.medico;
+              } )
+            )
+    } else {
+      // creando
+      url += '?token=' + this._usuarioService.token;
+      return this.http.post(url, medico)
+          .pipe(
+            map( (resp: any) => {
+  
+              swal('Created Doctor', medico.nombre, 'success');
+              return resp.medico;
+            })
+          );
+    }
+
   }
 
   cargarMedico( id: string ) {
